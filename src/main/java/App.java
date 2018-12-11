129a64345913bf131a92785c94d5a1ea244c5ffe
danhillcode <danhilltravel@hotmail.co.uk>
2018-12-11 13:35:07 +0000

    added connection to postgres with prepared statement

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -52,11 +52,71 @@ public class App{
         return null;
     }
 
+
+    public static void viewTable(Connection con) throws SQLException {
+
+        String query = "select COF_NAME, SUP_ID, PRICE, " +
+                "SALES, TOTAL " +
+                "from COFFEES";
+
+        try (Statement stmt = con.createStatement()) {
+
+            ResultSet rs = stmt.executeQuery(query);
+
+            while (rs.next()) {
+                String coffeeName = rs.getString("COF_NAME");
+                int supplierID = rs.getInt("SUP_ID");
+                float price = rs.getFloat("PRICE");
+                int sales = rs.getInt("SALES");
+                int total = rs.getInt("TOTAL");
+                System.out.println(coffeeName + ", " + supplierID +
+                        ", " + price + ", " + sales +
+                        ", " + total);
+            }
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+    }
+
+    public static void viewTable2(Connection con) throws SQLException {
+
+        String query = "Select * from company";
+        try (Statement stmt = con.createStatement()) {
+
+            ResultSet rs = stmt.executeQuery(query);
+
+            while (rs.next()) {
+                String salary = rs.getString("salary");
+               /* int supplierID = rs.getInt("SUP_ID");
+                float price = rs.getFloat("PRICE");
+                int sales = rs.getInt("SALES");
+                int total = rs.getInt("TOTAL");*/
+                System.out.println(salary);
+            }
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+
+    }
+
+
+
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
+
+        String url = "jdbc:postgresql://localhost:5431/";
+        String user = "danielhill";
+        String password = "";
+
+        Connection conn = null;
+        Class.forName("org.postgresql.Driver");
+        conn = DriverManager.getConnection(url, user, password);
+
         App app = new App();
-        app.connect();
+        app.viewTable2(conn);
+        app.viewTable2(conn);
     }
 }
